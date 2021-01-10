696544394157398521a2bb4009c14ee7359d7a0f
anthony <aahewins@protonmail.com>
2021-01-10 18:28:33 -0500

    auth changes

--- auth.go
+++ auth.go
@@ -2,6 +2,7 @@ package esri
 
 import (
 	"fmt"
+	"time"
 
 	"encoding/json"
 	"net/url"
@@ -15,9 +16,20 @@ var (
 	generateTokenURL = fmt.Sprintf("%v/sharing/rest/generateToken?f=json", referer)
 )
 
-type TokenResp struct {
+type tokenResp struct {
 	Token   string `json:"token"`
 	Expires int64  `json:"expires"`
+}
+
+// An AGOL token response
+type Token struct {
+	Token string
+	Expires time.Time
+}
+
+// Expired is a shorthand for telling whether or not a token is expired
+func (t Token) Expired() bool {
+	return !time.Now().Before(t)
 }
 
 func GetToken(username, password string) (*TokenResp, error) {
@@ -31,6 +43,24 @@ func GetToken(username, password string) (*TokenResp, error) {
 		return nil, err
 	}
 
-	var tokenStruct TokenResp
-	return &tokenStruct, json.Unmarshal(resp, &tokenStruct)
+	var t tokenResp
+	err = json.Unmarshal(resp, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Token{Token: t.Token, Expires: parseMilliseconds(t.Expires)}, nil
+}
+
+// parseMilliseconds parses a time in milliseconds and spits out a time.Time
+func parseMilliseconds(expiration int64) time.Time {
+	// Milliseconds elapsed since the epoch
+	//        1,000 micro sec     1,000 ns
+	// X ms * --------------- * -------------- = 1,000,000 * X ns
+	//           1 ms             1 micro sec
+
+	return time.Unix(
+		0,
+		expiration*1000000,
+	)
 }
